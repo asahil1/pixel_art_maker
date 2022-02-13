888e053e7b6a4a9346f78d5d00c7b52fc9e2de17
asahil1 <47396088+asahil1@users.noreply.github.com>
2022-02-12 23:02:32 -0800

    Add files via upload

--- designs.js
+++ designs.js
@@ -7,18 +7,18 @@ const form = document.querySelector('#sizePicker');
 const table = document.querySelector('#pixelCanvas');
 
 
-
+// Event listener for when Submit is clicked
 form.addEventListener('submit', function makeGrid() {
 
-        console.log('Submitted test');
+        console.log('Submitted test'); // Testing the listener
         event.preventDefault();
         const height = document.querySelector('#inputHeight').value;
         const width = document.querySelector('#inputWidth').value;
-        const color = document.querySelector('#colorPicker').value;
 
 
+        // This clears the table grids when form is submitted
         table.innerHTML = '';
-
+        // loops around elements and created table cells
         for (var r = 0; r < height; r++){
           const row = document.createElement('tr');
           table.appendChild(row);
@@ -29,11 +29,21 @@ form.addEventListener('submit', function makeGrid() {
 
           }
         }
-
+       // this will change the color of grid cell when clicked
         table.addEventListener('click',e => {
           if (e.target.matches('td')){
             const color = document.querySelector('#colorPicker').value;
             e.target.style.backgroundColor = color;
+
+          }
+
+        });
+
+        // this will erase a grid when double clicking it
+        table.addEventListener('dblclick',e => {
+          if (e.target.matches('td')){
+
+            e.target.style.backgroundColor = 'white';
 
           }
 
